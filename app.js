4992eb15171e30608cf9dc8176b61480de62252c
sardartejpal <sardartejpal@gmail.com>
2020-07-19 14:41:43 +0530

    Documented properly. Added comments

--- app.js
+++ app.js
@@ -10,46 +10,50 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
-// Creating map for storing randomly generated strigs for user authentication
-let map = new Map();
-
-//For using the ejs module for the usage of templating.
+// Use ejs module for templating.
 app.set("view engine", "ejs");
 
 // Body Parser
-// To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser.
-
-// body-parser extract the entire body portion of an incoming request stream and exposes it on req.body.
-
-// This body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request. Install body-parser using NPM as shown below
+// Body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request.
+// ?
+// Use body-parser for parsing the data 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // For asking express to load the local static files like css and others because by default it does not load them.
+// Load static files
 app.use(express.static("public"));
 
 
-// Connecting to mongo db and creating database
+// Connect to mongo db and create userDB database
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Createing Schema  
+// Create Schema  
 const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
-    email: String,
-    password: String
+    // email and password as required while registering
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
-// registering encrypt plugin to our schema to for encryption of our database
-// const secret = "Thisisourlittlesecret";
+// register encrypt plugin to our schema to for encryption of our password field
 userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
-// Setting up a new user model. Creation of User collection using user schema
+// ?
+// Set a new user model. Creation of User collection using user schema
 const User = new mongoose.model("User", userSchema);
 
-
+// Render home page for the home(/) route
 app.get("/", function (req, res) {
     res.render("home");
 });
+
 
 app.get("/users/register", function (req, res) {
     res.render("register");
@@ -59,77 +63,91 @@ app.get("/users/login", function (req, res) {
     res.render("login");
 });
 
-
+// User registration 
 app.post("/users/register", function (req, res) {
-
-    // Generating a random string token for user with min length 20
-    // let newid = makeid((Math.random() * 30) + 20.);
+    // Creating a new document for new user.
     const newUser = new User({
-        // id: newid,
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         password: req.body.password
     });
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
+    // Check if the user already exist in the collection
+    newUser.findOne({email: email}, function(err, user){
+        if(user){
+            res.send("User exists");
         } else {
-            // res.render("secrets");
-            // res.json({
-            //     token: newid
-            // });
-            jwt.sign({ email: newUser.email }, process.env.SECRET, function (err, token) {
-                res.json({
-                    token: token
-                }).sendStatus(200);
+            // Saving the document in User collection
+            newUser.save(function (err) {
+                if (err) {
+                    // If any error send the error.
+                    res.send(err);
+                } else {
+                    // Creating a string token with help of JWT using default RSA encryption algorithm
+                    jwt.sign({ email: newUser.email }, process.env.SECRET, function (err, token) {
+                        // Should I write err statement here?
+                        // Sending token as response to user if there were no errors
+                        res.json({
+                            token: token
+                        }).sendStatus(200);
+                    });
+                }
             });
         }
     });
 });
 
+// User login
 app.post("/users/login", function (req, res) {
-    
+    // User login with email and password
     const email = req.body.email;
     const password = req.body.password;
+    // Searching User collection with email.
     User.findOne({ email: email }, function (err, foundUser) {
         if (err) {
-            console.log(err);
+            res.send(err);
         } else {
             if (foundUser) {
+                // Decrypts the passowrd and checks if the entered password is same.
                 if (foundUser.password === password) {
-                    // res.render("secrets");
+                    // Creating a string token with help of JWT using default RSA encryption algorithm
                     jwt.sign({ email: email }, process.env.SECRET, function (err, token) {
                         res.json({
                             token: token
                         });
                     })
                 } else {
+                    // If the password is incorrect, sending unauthorized response
                     res.sendStatus(401);
                 }
             } else {
+                // If user not found sending 404 not found response
                 res.sendStatus(404);
             }
         }
     });
 });
 
+// User Update
+// Extracting the token with verifyToken function from request header, authorization.
 app.patch("/users/update", verifyToken, function (req, res) {
+    // Verifying the jwt token, if correct we get decoded payload.
     jwt.verify(req.token, process.env.secret, function (err, decoded) {
         if (err) {
+            // Reponding unauthorized 401 error
             res.sendStatus(401);
         } else {
-            // res.write("Post created");
-            // res.write(authData);
-            // res.send();
+            // Searching with help of email in the User collection.
             User.updateOne({ email: decoded.email },
                 {
+                    // Updating the firstname and lastname in the documnet.
                     $set: {
                         firstName: req.body.firstName,
                         lastName: req.body.lastName
                     }
                 }, function (err) {
                     if (err) {
+                        // If email not found, responding unauthorized 401 error.
                         res.sendStatus(401);
                     } else {
                         res.send("Successfully updated");
@@ -140,13 +158,19 @@ app.patch("/users/update", verifyToken, function (req, res) {
 
 });
 
+// User deletion
+// Extracting JWT token with verifyToken function
 app.delete("/users/delete", verifyToken, function (req, res) {
+    // Verifying the token
     jwt.verify(req.token, process.env.secret, function (err, decoded) {
         if (err) {
+            // If error, responding with unauthorized 401 error.
             res.sendStatus(401);
         } else {
+            // Delete the user from User collection, searching with email.
             User.deleteOne({ email: decoded.email }, function (err) {
                 if (err) {
+                    // Unauthorized
                     res.sendStatus(401);
                 } else {
                     res.send("Successfully Deleted");
@@ -157,17 +181,7 @@ app.delete("/users/delete", verifyToken, function (req, res) {
 
 });
 
-// function makeid(length) {
-//     var result = '';
-//     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//     var charactersLength = characters.length;
-//     for (var i = 0; i < length; i++) {
-//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     }
-//     return result;
-// }
-
-
+// Listening to the port 3000.
 app.listen(3000, function () {
     console.log("Server started on port 3000!");
 });
@@ -177,7 +191,7 @@ app.listen(3000, function () {
 // Verify Token
 // next parameter is used for proceding the execution from where it was called
 function verifyToken(req, res, next) {
-    // Get auth header value
+    // Get authorization header value
     const bearerHeader = req.headers["authorization"];
     // check if bearer is undefined
     if (typeof bearerHeader !== "undefined") {
@@ -190,7 +204,7 @@ function verifyToken(req, res, next) {
         // next middleware
         next();
     } else {
-        // Forbidden
-        res.sendStatus(403);
+        // Responding Unauthorizer 401 error
+        res.sendStatus(401);
     }
 }
